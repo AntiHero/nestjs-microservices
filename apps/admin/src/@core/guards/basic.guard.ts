35b5ddfc87d70ca655730edc8f0ec4ae94525a47
Aleksandr Schemelev <prophreaker@gmail.com>
2023-06-13 14:27:37 +0300

    feat(admin): check if route is public

--- apps/admin/src/@core/guards/basic.guard.ts
+++ apps/admin/src/@core/guards/basic.guard.ts
@@ -1,18 +1,32 @@
+import { IS_PUBLIC_KEY } from '@app/common/decorators/public.decorator';
 import {
   Injectable,
   CanActivate,
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
 import { AuthService } from '../../auth/auth.service';
 
 @Injectable()
 export class BasicAuthGuard implements CanActivate {
-  public constructor(private readonly authService: AuthService) {}
+  public constructor(
+    private readonly authService: AuthService,
+    private readonly reflector: Reflector,
+  ) {}
 
   public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const req = GqlExecutionContext.create(context)?.getContext()?.req;
 
     const [schema, payload] = req.headers['authorization']?.split(' ') || [];

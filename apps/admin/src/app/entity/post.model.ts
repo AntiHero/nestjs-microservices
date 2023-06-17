86dacf6b0440375c1d787fa48753c32580d87d36
Aleksandr Schemelev <prophreaker@gmail.com>
2023-06-17 15:24:36 +0300

    feat: create PostModel and PostsQueryRepository

---
+++ apps/admin/src/app/entity/post.model.ts
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import type { ImageMetadata, Image, Post } from '@prisma/client';
+import { prop } from '@typegoose/typegoose';
+
+export interface PostModel extends Base {}
+export class PostModel
+  extends TimeStamps
+  implements Record<Exclude<keyof Post, 'createdAt' | 'updatedAt'>, any>
+{
+  @prop({ unique: true })
+  public id: string;
+
+  @prop()
+  public userId: string;
+
+  @prop()
+  public description: string;
+
+  @prop(() => [ImageModel])
+  public images: ImageModel[];
+}
+
+export interface ImageModel extends Base {}
+export class ImageModel
+  extends TimeStamps
+  implements Record<Exclude<keyof Image, 'createdAt' | 'updatedAt'>, any>
+{
+  @prop({ unique: true })
+  public id: string;
+
+  @prop()
+  public url: string;
+
+  @prop({ default: null })
+  public previewUrl: string | null;
+
+  @prop()
+  public postId: string;
+
+  @prop(() => MetadataModel)
+  public metadata: MetadataModel;
+}
+
+export interface MetadataModel extends Base {}
+export class MetadataModel
+  extends TimeStamps
+  implements
+    Record<Exclude<keyof ImageMetadata, 'createdAt' | 'updatedAt'>, any>
+{
+  @prop({ unique: true })
+  public id: string;
+
+  @prop()
+  public size: number;
+
+  @prop()
+  public height: number;
+
+  @prop()
+  public width: number;
+
+  @prop()
+  public imageId: string;
+}
---
+++ apps/admin/src/database/posts.query-repository.ts
@@ -0,0 +1,18 @@
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { Injectable, Provider } from '@nestjs/common';
+
+import { InjectModel } from 'nestjs-typegoose';
+import { PostModel } from '../app/entity/post.model';
+import { MongoQueryRepository } from './abstracts/mongo.query-repository';
+
+@Injectable()
+export class PostsQueryRepository extends MongoQueryRepository<PostModel> {
+  public constructor(@InjectModel(PostModel) repository: ModelType<PostModel>) {
+    super(repository);
+  }
+}
+
+export const UsersQueryRepositoryProvider: Provider = {
+  provide: MongoQueryRepository,
+  useClass: PostsQueryRepository,
+};

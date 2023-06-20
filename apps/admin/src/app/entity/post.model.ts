1a47d1b2ebe27eca7d956e754d707f6a97c94652
Aleksandr Schemelev <prophreaker@gmail.com>
2023-06-20 18:15:46 +0300

    fix(admin): mixed types

--- apps/admin/src/app/entity/post.model.ts
+++ apps/admin/src/app/entity/post.model.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
-import type { ImageMetadata, Image, Post } from '@prisma/client';
+import type { Image, Post } from '@prisma/client';
 import { prop } from '@typegoose/typegoose';
 
 // export interface MetadataModel extends Base {}
@@ -36,7 +36,7 @@ export class ImageModel
   @prop()
   public url: string;
 
-  @prop({ default: null })
+  @prop({ type: () => String, default: null })
   public previewUrl: string | null;
 
   @prop()
@@ -60,6 +60,6 @@ export class PostModel
   @prop()
   public description: string;
 
-  @prop(() => [ImageModel])
+  @prop({ type: () => [ImageModel], default: [] })
   public images: ImageModel[];
 }

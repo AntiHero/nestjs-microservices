c95bbb33e5c1d995a05279d2e4adcf0f4830bb17
Aleksandr Schemelev <prophreaker@gmail.com>
2023-07-18 23:36:16 +0300

    feat(admin): add isBanned field to UserOutput

--- apps/admin/src/utils/user-list-view.mapper.ts
+++ apps/admin/src/utils/user-list-view.mapper.ts
@@ -5,11 +5,13 @@ export interface UserViewModel {
   username: string;
   profileLink: string;
   dateAdded: string;
+  isBanned: boolean;
 }
 
 export const toUserViewModel = (input: UserClass): UserViewModel => ({
   id: input.id,
   username: input.username,
+  isBanned: input.isBanned,
   profileLink: `${process.env.FRONTEND_DOMAIN}/users/${input.username}`,
   dateAdded: input.createdAt as unknown as string,
 });
